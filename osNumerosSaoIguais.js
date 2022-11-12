13b5a32065855150684f08c1f8de9dd9dd854187
Jonatan Rocha <67324882+JonatanRocha2@users.noreply.github.com>
2022-11-12 19:43:09 -0300

    Create osNumerosSaoIguais.js

---
+++ osNumerosSaoIguais.js
@@ -0,0 +1,26 @@
+// IMPORTANTE: As funções "gets" e "print" são acessíveis globalmente, onde: 
+// - "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+// - "print": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente.
+
+let A = gets();
+let B = gets();
+
+//TODO: Imprima se os valores numéricos passados são iguais ou não.
+
+function verificaNumeros(A, B) {
+  
+  if(A == B){
+    
+    return "Sao iguais!"
+    
+  }
+  
+  else {
+    
+    return "Nao sao iguais!"
+    
+  }
+  
+}
+
+print(verificaNumeros(A, B))
